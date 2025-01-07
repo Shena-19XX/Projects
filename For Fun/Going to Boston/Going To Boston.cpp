6c70f032c390476e4904256848f2f6e42a8b455f
Shena J <codeworld400@mail.com>
2025-01-06 20:43:57 -0600

    Remove Other Game Play

--- For Fun/Going to Boston/Going To Boston.cpp
+++ For Fun/Going to Boston/Going To Boston.cpp
@@ -10,7 +10,6 @@ int genRandom(int upper, int lower) {
 
 int main()
 {
-   // Problem 2
     srand(time(0));
     const int UPPER = 6;
     const int LOWER = 1;
@@ -56,53 +55,4 @@ int main()
       cout << "It's a tie!" << endl;
     }
 
-    // Problem 3 keeping necessary code from Problem 2
-    cout << endl << "Starting Game 2: " << endl << endl;
-    player1 = player2 = 5;
-    pot = 21;
-    d1 = d2 = 0;
-    while (player1 > 0 && player2 > 0) {
-      cout << "Player 1's turn: " << endl;
-      d1 = genRandom(UPPER, LOWER);
-      cout << "Die 1: " << d1 << endl;
-      d2 = genRandom(UPPER, LOWER);
-      cout << "Die 2: " << d2 << endl;
-      if (d1 + d2 == 7) {
-         player1 += 7;
-         cout << "Player 1 hits 7!" << endl;
-      }
-      else {
-         int diff = abs((d1+d2) - 7);
-         player1 -= diff;
-      }
-      cout << "Player 2's turn: " << endl;
-      d1 = genRandom(UPPER, LOWER);
-      d2 = genRandom(UPPER, LOWER);
-      cout << "Die 1: " << d1 << endl;
-      cout << "Die 2: " << d2 << endl;
-      if (d1 + d2 == 7) {
-         player2 += 7;
-         cout << "Player 2 hits 7!" << endl;
-      }
-      else {
-         int diff = abs((d1+d2) - 7);
-         player2 -= diff;
-      }
-      cout << "Current score: " << endl;
-      cout << "Player 1: " << player1 << endl;
-      cout << "Player 2: " << player2 << endl;
-    }
-    cout << "Final score: " << endl;
-    cout << "Player 1: " << player1 << endl;
-    cout << "Player 2: " << player2 << endl;
-    if (player1 > player2) {
-       cout << "Player 1 wins!" << endl;
-    }
-    else if (player2 > player1) {
-      cout << "Player 2 wins!" << endl;
-    }
-    else {
-      cout << "It's a tie!" << endl;
-    }
-    return 0;
-}
+} 
